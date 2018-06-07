b5d5a6ebba17d34997fcf0e4e87d2862e931be46
Andri Danusasmita <andridsasmita@gmail.com>
2018-06-07 17:58:22 +0900

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -19,3 +19,21 @@ def train_test_split(X, y, test_size, random_state=42):
     y_train, y_test = y[:-n_test], y[-n_test:] # split y into y_train and y_test
 
     return X_train, X_test, y_train, y_test
+
+def Kfold(X, n_splits, shuffle=False, random_state=42):
+    """K-Fold Cross Validation in Numpy."""
+    idx = np.arange(X.shape[0])
+    if shuffle:
+        print('Shuffling with random seed: {}'.format(random_state))
+        np.random.seed(random_state) # for reproducibility
+        idx = np.random.permutation(X.shape[0]) # shuffling row indices
+    len_test_fold = int(X.shape[0] / n_splits) # no of test datapoints per fold
+    for i in range(n_splits):
+        test_fold_idx = idx[np.arange(len_test_fold*i, len_test_fold*(i+1), 1)] # list of test fold indices
+        train_fold_idx = np.setdiff1d(np.arange(X.shape[0]), test_fold_idx) # list of train fold indices
+        
+        yield train_fold_idx, test_fold_idx
+        
+
+
+
