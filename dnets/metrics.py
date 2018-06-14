973c948a0ee85a98c0d876cf3a77fa6b46988d80
Andri Danusasmita <25168506+andridns@users.noreply.github.com>
2018-06-14 19:01:08 +0900

    Update metrics.py

--- dnets/metrics.py
+++ dnets/metrics.py
@@ -11,3 +11,12 @@ def accuracy(preds, y):
 def mean_squared_error(preds, y):
     """Returns mean squared error (MSE) between predictions and labels."""
     return np.mean((y-preds)**2)
+
+def mean_absolute_error(preds, y):
+    """Returns mean absolute error (MAE) between predictions and labels."""
+    return np.mean(np.abs(y-preds))
+
+def mean_absolute_percentage_error(preds, y):
+    """Returns mean absolute percentage error (MAPE) between predictions and labels."""
+    diff = np.abs((y-preds) / (y+1e-8))
+    return 100. * np.mean(diff)
