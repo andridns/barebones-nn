def5189f3b51da7f7746f755657f329eb212a915
Andri Danusasmita <25168506+andridns@users.noreply.github.com>
2018-06-14 18:58:25 +0900

    Update metrics.py

--- dnets/metrics.py
+++ dnets/metrics.py
@@ -5,5 +5,9 @@ def cat_xentropy_loss(probs, y):
     return np.mean(-np.log(probs[np.arange(y.shape[0]), y]))
 
 def accuracy(preds, y):
-    """Returns accuracy score given prediction probabilities and labels"""
+    """Returns accuracy score given prediction probabilities and labels."""
     return np.sum(preds==y) / float(len(y))
+
+def mean_squared_error(preds, y):
+    """Returns mean squared error (MSE) between predictions and labels."""
+    return np.mean((y-preds)**2)
